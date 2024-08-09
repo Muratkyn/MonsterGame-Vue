21010eb9d5b37c5a194bb31e6e09974d8ba95075
muratkoyun <mkoyun@red.software.systems>
2024-08-09 17:37:37 +0200

    finalizing app

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
       monsterHealth: 100,
       clickCount: 0,
       enableButton: true,
+      winner: null,
     };
   },
   watch: {
@@ -18,12 +19,32 @@ const app = Vue.createApp({
       }
       return (this.enableButton = true);
     },
+    playerHealth(val) {
+      if (val <= 0 && this.monsterHealth <= 0) {
+        this.winner = "Draw";
+      } else if (val <= 0) {
+        this.winner = "Monster";
+      }
+    },
+    monsterHealth(val) {
+      if (val <= 0 && this.playerHealth <= 0) {
+        this.winner = "Draw";
+      } else if (val <= 0) {
+        this.winner = "Player";
+      }
+    },
   },
   computed: {
     monsterBarStyle() {
+      if (this.monsterHealth < 0) {
+        return { width: 0 };
+      }
       return { width: `${this.monsterHealth}%` };
     },
     playerBarStyle() {
+      if (this.playerHealth < 0) {
+        return { width: 0 };
+      }
       return { width: `${this.playerHealth}%` };
     },
   },
@@ -56,6 +77,16 @@ const app = Vue.createApp({
         return (this.playerHealth = 100);
       }
       return (this.playerHealth += healNumber);
+    },
+    startNewGame() {
+      this.monsterHealth = 100;
+      this.playerHealth = 100;
+      this.clickCount = 0;
+      this.winner = null;
+    },
+    surrender() {
+      this.playerHealth = 0;
+      this.winner = "Monster";
     },
   },
 });
